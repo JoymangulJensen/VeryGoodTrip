ffe2e1c67fc7d74bfe774bfb928006058eac3d83
GaetanMartin <gaetan.martin1993@gmail.com>
2016-03-15 15:27:15 +0100

    Modification du contenu de la base de données

--- db/content.sql
+++ db/content.sql
@@ -1,6 +1,7 @@
 INSERT INTO `category` (`category_id`, `category_name`, `category_description`, `category_image`) VALUES
 (1, 'Europe', 'Occident', './images/france.jpg'),
-(2, 'Amérique du Nord', 'Visitez les Etats-Unis ou encore le Canada au meilleur prix', './images/amerique.jpg');
+(2, 'Amérique du Nord', 'Visitez les Etats-Unis ou encore le Canada au meilleur prix', './images/amerique.jpg'),
+(3, 'Asie', 'L''Asie est délimitée par l''océan Arctique au nord, l''océan Pacifique à l''est, l''océan Indien au sud, l''océan Indien (mer Rouge) au sud-ouest, l''océan Atlantique (mer Méditerranée et mer Noire) et la mer Caspienne...', './images/asie.jpg');
 
 INSERT INTO `trip` (`trip_id`, `trip_name`, `trip_description`, `trip_price`, `trip_image`, `category_id`) VALUES
 (1, 'France', 'La France est merveilleuse !', 500, './images/france.jpg', 1),
